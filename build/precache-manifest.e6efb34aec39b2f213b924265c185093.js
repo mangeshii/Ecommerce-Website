cb1ecc701083b7f2d3fbc10d15b12b75550f19a6
mangeshipatil <thisismangeshi@gmail.com>
2022-07-06 15:52:58 +0530

    added build folder

---
+++ build/precache-manifest.e6efb34aec39b2f213b924265c185093.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cd26f23122a41e45cd6",
+    "url": "/static/css/main.e3aad53b.chunk.css"
+  },
+  {
+    "revision": "1cd26f23122a41e45cd6",
+    "url": "/static/js/main.1cd26f23.chunk.js"
+  },
+  {
+    "revision": "cf46052090ec38fe42f6",
+    "url": "/static/css/1.184bdccf.chunk.css"
+  },
+  {
+    "revision": "cf46052090ec38fe42f6",
+    "url": "/static/js/1.cf460520.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "621ec448d534e60e4b5148b4557fd491",
+    "url": "/index.html"
+  }
+];
